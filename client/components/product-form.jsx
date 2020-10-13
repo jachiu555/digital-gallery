135f6b34e49f47e0c1f54659475377a71378e6df
James Chiu <jachiu555@gmail.com>
2020-10-13 04:25:03 +0000

    After creating the form and stylizing it a bit, made it so user inputs are saved in state, making them controlled components. Added regex so user can only input numbers for the price section.

--- client/components/product-form.jsx
+++ client/components/product-form.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
 export default class ProductForm extends React.Component {
-  // constructor(props) {
-  //   super(props);
+  constructor(props) {
+    super(props);
 
-  // }
+    this.state = {
+      title: '',
+      price: '',
+      description: '',
+      upload: ''
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onPriceChange = this.onPriceChange.bind(this);
+    this.onDescriptionChange = this.onDescriptionChange.bind(this);
+  }
+
+  onTitleChange(event) {
+    this.setState({
+      title: event.target.value
+    });
+  }
+
+  onPriceChange(event) {
+    const re = /^[0-9\b]+$/;
+
+    if (event.target.value === '' || re.test(event.target.value)) {
+      this.setState({
+        price: event.target.value
+      });
+    }
+  }
+
+  onDescriptionChange(event) {
+    this.setState({
+      description: event.target.value
+    });
+  }
 
   render() {
     return (
@@ -12,15 +44,15 @@ export default class ProductForm extends React.Component {
         <form>
           <div className="form-inline justify-content-center mb-5">
             <label className="mr-5 formTextSize" htmlFor="formGroupExampleInput">Title</label>
-            <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"></input>
+            <input value={this.state.title} onChange={this.onTitleChange} type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"></input>
           </div>
           <div className="form-inline justify-content-center mb-5">
             <label className="mr-5 formTextSize" htmlFor="formGroupExampleInput2">Price</label>
-            <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"></input>
+            <input value={this.state.price} onChange={this.onPriceChange} type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"></input>
           </div>
           <div className="form-inline justify-content-center mb-5">
             <label className="mr-5 formTextSize" htmlFor="formGroupExampleInput2">Description</label>
-            <input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"></input>
+            <input value={this.state.description} onChange={this.onDescriptionChange} type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"></input>
           </div>
           <div className="pl-4 form-inline justify-content-center">
             <label className="mr-5 formTextSize" htmlFor="exampleFormControlFile1">Example file input</label>
