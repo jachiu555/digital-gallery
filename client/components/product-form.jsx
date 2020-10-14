39daabc4fb11354f3dba1034b2c5997777cecc91
James Chiu <jachiu555@gmail.com>
2020-10-14 06:17:59 +0000

    Fixed some more modal stylings. Form now has proper functionality of saving user input into state. Working on the upload image part now.

--- client/components/product-form.jsx
+++ client/components/product-form.jsx
@@ -14,6 +14,8 @@ export default class ProductForm extends React.Component {
     this.onTitleChange = this.onTitleChange.bind(this);
     this.onPriceChange = this.onPriceChange.bind(this);
     this.onDescriptionChange = this.onDescriptionChange.bind(this);
+    this.onUploadImage = this.onUploadImage.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   onTitleChange(event) {
@@ -38,10 +40,30 @@ export default class ProductForm extends React.Component {
     });
   }
 
+  onUploadImage(event) {
+    const [file] = event.target.files;
+
+    const fr = new FileReader();
+
+    fr.readAsDataURL(file);
+    fr.addEventListener('uploaded', () => {
+      this.setState({
+        upload: fr.result
+      });
+    });
+  }
+
+  handleSubmit(event) {
+    // console.log(this.state);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className='container modalContainer'>
-        <form>
+        <h1 className="text-center">Add an image</h1>
+        <br></br>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-inline justify-content-center mb-5">
             <label className="mr-5 formTextSize" htmlFor="formGroupExampleInput">Title</label>
             <input value={this.state.title} onChange={this.onTitleChange} type="text" className="form-control" id="formGroupExampleInput" placeholder="Example input"></input>
@@ -54,9 +76,13 @@ export default class ProductForm extends React.Component {
             <label className="mr-5 formTextSize" htmlFor="formGroupExampleInput2">Description</label>
             <input value={this.state.description} onChange={this.onDescriptionChange} type="text" className="form-control" id="formGroupExampleInput2" placeholder="Another input"></input>
           </div>
-          <div className="pl-4 form-inline justify-content-center">
-            <label className="mr-5 formTextSize" htmlFor="exampleFormControlFile1">Example file input</label>
-            <input type="file" className="form-control-file formTextSize" id="exampleFormControlFile1"></input>
+          <div className="custom-file mb-5">
+            <input result={this.state.upload} onChange={this.onUploadImage} type="file" className="custom-file-input" id="customFile"></input>
+            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+          </div>
+
+          <div className="d-flex justify-content-center buttonContainer">
+            <input className="btn btn-primary mb-5" type="submit" value="Submit"></input>
           </div>
         </form>
       </div>
