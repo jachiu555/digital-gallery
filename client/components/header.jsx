fff9b5aac486c4ec20e153647fc3db234fc1ffb5
James Chiu <jachiu555@gmail.com>
2020-11-26 03:51:15 +0000

    Added functionality to Digital Gallery text in Header component to direct back to catalog.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.setView('catalog', {});
+  }
 
   render() {
     return (
       <div>
         <header>
           <div className="d-flex">
-            <h3 className="mr-auto p-2">Digital Gallery</h3>
+            <h3 className="mr-auto p-2" onClick={this.handleClick}>Digital Gallery</h3>
             <i className="fas fa-user-plus p-2 signUp"> Sign Up</i>
             <i className="fas fa-sign-in-alt p-2 signIn"> Sign In</i>
             <i className="fas fa-sign-out-alt p-2 signOut"> Sign Out</i>
