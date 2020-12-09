d131a963e52a2d4d0fe4cf663fb6a00fbe22718f
James Chiu <jachiu555@gmail.com>
2020-12-09 04:24:14 +0000

    Fixed styling for ProductForm component exit button and placeholder text. Added functionality to SignIn in Header to direct to SignInForm component.

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -26,7 +26,7 @@ export default class Header extends React.Component {
       <div>
         <header>
           <div className="d-flex">
-            <h3 className="mr-auto p-2" onClick={this.handleClickGallery}>Digital Gallery</h3>
+            <h3 className="mr-auto" onClick={this.handleClickGallery}>Digital Gallery</h3>
             <i className="fas fa-user-plus p-2 signUp" onClick={this.handleClickSignUp}> Sign Up</i>
             <i className="fas fa-sign-in-alt p-2 signIn" onClick={this.handleClickSignIn}> Sign In</i>
             <i className="fas fa-sign-out-alt p-2 signOut"> Sign Out</i>
