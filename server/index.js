cc00db59058a30e2f49c60a49ccb4ea1fcce4c84
James Chiu <jachiu555@gmail.com>
2020-12-17 07:19:54 +0000

    Working to fix app.get() to api/users/userId.

--- server/index.js
+++ server/index.js
@@ -53,11 +53,11 @@ app.get('/api/products/:productid', (req, res, next) => {
     });
 });
 
-app.get('api/users', (req, res, next) => {
+app.get('/api/users/:userId', (req, res, next) => {
   db.query('select * from users where userId = $1', [req.params.userId])
     .then(result => {
       if (!result.rows[0]) {
-        return next(new ClientError(`cannot find user with id ${req.params.userId}`, 404));
+        return next(new ClientError(`cannot find product with id ${req.params.userId}`, 404));
       } else {
         res.json(result.rows[0]);
       }
