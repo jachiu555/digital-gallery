3b1c19f56498268ee9b32c698b88feaf638fa25f
James Chiu <jachiu555@gmail.com>
2020-12-22 08:19:23 +0000

    Working on making users list array show up in an option tag ono SignInForm component.

--- client/components/signin-form.jsx
+++ client/components/signin-form.jsx
@@ -5,24 +5,35 @@ export default class SignInForm extends React.Component {
     super(props);
 
     this.state = {
-      userName: '',
-      password: ''
+      // userName: '',
+      // password: ''
+      users: []
     };
 
-    this.onUserNameChange = this.onUserNameChange.bind(this);
+    // this.onUserNameChange = this.onUserNameChange.bind(this);
     this.onPasswordChange = this.onPasswordChange.bind(this);
   }
 
-  onUserNameChange(event) {
-    this.setState({
-      userName: event.target.value
-    });
-  }
+  // onUserNameChange(event) {
+  //   this.setState({
+  //     userName: event.target.value
+  //   });
+  // }
 
   onPasswordChange(event) {
     this.setState({
       password: event.target.value
     });
+  }
+
+  getUsers() {
+    fetch('/api/users')
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          users: data
+        }))
+      .catch(error => console.error(error));
   }
 
   handleSubmit(event) {
@@ -40,23 +51,28 @@ export default class SignInForm extends React.Component {
   }
 
   render() {
+    const userArray = this.state.users.map(users => <option key={users}>{users}</option>);
+
     return (
       <form className="container d-flex justify-content-center">
         <div className="form-group align-items-center">
           <div className="col-md-12 my-3">
             <h1 className="title">Sign In</h1>
-            <label className="sr-only" htmlFor="inlineFormInputGroupUsername">Username</label>
+            {/* <label className="sr-only" htmlFor="inlineFormInputGroupUsername">Username</label>
             <div className="input-group">
               <div className="input-group-prepend">
                 <div className="input-group-text">@</div>
               </div>
               <input type="text" className="form-control" id="inlineFormInputGroupUsername" placeholder="Username"></input>
-            </div>
+            </div> */}
+            <select className="custom-select mr-sm-2" id="inlineFormCustomSelect">
+              {userArray}
+            </select>
           </div>
-          <div className="col-md-12 my-3">
+          {/* <div className="col-md-12 my-3">
             <label className="sr-only" htmlFor="inlineFormInputName">Password</label>
             <input type="text" className="form-control" id="inlineFormInputName" placeholder="Password"></input>
-          </div>
+          </div> */}
           <div className="col-auto my-1">
             <div className="form-check">
               <input className="form-check-input" type="checkbox" id="autoSizingCheck2"></input>
